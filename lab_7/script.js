76f862fb7205582994f57cf63152670accbcfe83
ahsankhalid180 <90290806+ahsankhalid180@users.noreply.github.com>
2022-12-09 22:33:57 -0500

    edit

--- lab_7/script.js
+++ lab_7/script.js
@@ -101,11 +101,11 @@ async function mainEvent() {
       Dot notation is preferred in JS unless you have a good reason to use brackets
       The 'data' key, which we set at line 38 in foodServiceRoutes.js, contains all 1,000 records we need
     */
-  console.table(arrayFromJson.data);
+  console.table(arrayFromJson);
 
   // in your browser console, try expanding this object to see what fields are available to work with
   // for example: arrayFromJson.data[0].name, etc
-  console.log(arrayFromJson.data[0]);
+  console.log(arrayFromJson[0]);
 
   // this is called "string interpolation" and is how we build large text blocks with variables
   console.log(`${arrayFromJson.data[0].name} ${arrayFromJson.data[0].category}`);
